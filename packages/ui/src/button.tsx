d73ff1d16539f8aefa365929d6ed7d812d6e40ee
Resham <reshamupadhyay48@gmail.com>
2025-08-07 22:23:55 +0530

    ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,7 +11,7 @@ interface ButtonProps {
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={'text-purple-900 bg-yellow-200 p-5 rounded-full cursor-pointer'}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
